bff7f61e6233938fdc69546ea38a3ee2904e81f1
tunght91 <hoangtung.utc@gmail.com>
2022-02-11 16:56:31 +0700

    update

--- test/MOLToken.test.js
+++ test/MOLToken.test.js
@@ -1,9 +1,6 @@
-/* libraries used */
-const truffleAssert = require('truffle-assertions');
-/* Contracts in this test */
 const MOLToken = artifacts.require('../contracts/MOLToken.sol');
 
-contract("CyBlocCore", ([owner]) => {
+contract("CyBlocCore", ([owner, bob, tom]) => {
   
   before(async () => {
     token = await MOLToken.new({ from: owner })
@@ -11,8 +8,40 @@ contract("CyBlocCore", ([owner]) => {
 
   describe('#constructor()', () => {
     it('should set the token name, symbol, and URI', async () => {
-      const symbol = await cyblocCore.symbol();
+      const symbol = await token.symbol();
       assert.equal(symbol, 'MOL');
+
+      await token.mint(bob, 100, { from: owner })
+
+      var balance = await token.balanceOf(bob)
+
+      assert.equal(balance.toNumber(), 100)
+
+      try {
+        await token.mint(bob, 100, { from: bob })
+        assert.equal(1, 0)
+      }
+      catch (ex) {
+      }
+
+      await token.transfer(tom, 50, { from: bob })
+
+      balance = await token.balanceOf(tom)
+      assert.equal(balance.toNumber(), 50)
+
+      await token.pause()
+
+      try {
+        await token.transfer(tom, 10, { from: bob })
+        assert.equal(1, 0)
+      }
+      catch (ex) {}
+
+      await token.unPause()
+
+      await token.transfer(tom, 10, { from: bob })
+      balance = await token.balanceOf(tom)
+      assert.equal(balance.toNumber(), 60)
     });
   });
 });
