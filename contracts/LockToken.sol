597012961368cc414829cf23f82b7521a6db23b7
tunght91 <hoangtung.utc@gmail.com>
2022-03-25 16:06:24 +0700

    update token lock

--- contracts/LockToken.sol
+++ contracts/LockToken.sol
@@ -17,7 +17,7 @@ contract LockToken {
     IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
     amounts[token][msg.sender] = amounts[token][msg.sender].add(amount);
 
-    history[token][msg.sender].push(amount * 10**12 + block.timestamp);
+    history[token][msg.sender].push(amount * 10**18 + block.timestamp * 1000 + 1);
   }
 
   function withdraw(address token, uint amount) public {
@@ -26,6 +26,22 @@ contract LockToken {
     amounts[token][msg.sender] = amounts[token][msg.sender].sub(amount);
     IERC20(token).safeTransfer(msg.sender, amount);
 
-    history[token][msg.sender].push(block.timestamp);
+    history[token][msg.sender].push(amount * 10**18 + block.timestamp * 1000);
+  }
+
+  function getHistory(address token, address add, uint limit) public view returns (uint lastIndex, uint[] memory amount, uint[] memory time) {
+    uint n = history[token][add].length;
+    uint nitem = n > limit ? limit : n;
+
+    amount = new uint[](nitem);
+    time = new uint[](nitem);
+
+    for (uint i = 1; i <= nitem; i++) {
+      uint v = history[token][add][n - i];
+      amount[i - 1] = v / (10 ** 18);
+      time[i - 1] = v % (10 ** 18);
+    }
+
+    lastIndex = n - nitem;
   }
 }
