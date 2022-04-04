c5d7398980a0eda263eacd36f1d9e17c6c4691a6
tunght91 <hoangtung.utc@gmail.com>
2022-04-04 13:41:12 +0700

    update

--- contracts/MollectorCard.sol
+++ contracts/MollectorCard.sol
@@ -11,31 +11,45 @@ contract MollectorCard is MollectorCardBase {
         address add;
     }
 
-    uint[] public DNAs;
+    uint[] public DNAs; /// 10002000120034002300
 
     NFTLink[] public NFTLinks;
     mapping(uint => mapping(uint => uint)) CardLinks; // MolTokenId => NFT Contract Index => NFT Contract's tokenId
 
-    address[] public spawnerList;
-    mapping(address => bool) spanwer;
-
-    address[] public managerList;
-    mapping(address => bool) manager;
-
-    address[] public linkerList;
-    mapping(address => bool) linker;
-
+    address[] public operators;
+    mapping(address => bool) operator;
 
     event Spawned(uint256 indexed _tokenId, address indexed _owner, uint256 _dna);
     event Updated(uint256 indexed _tokenId, uint256 _dna);
     event Link(uint indexed _tokenId, uint _nftLinkIndex, uint _nftLinkTokenId);
 
-    constructor(address _owner) ERC721("CyBloc", "BLOC") AccessControl(_owner) {
+    constructor(address _owner) ERC721("Mollector Card", "MOLCARD") {
     }
 
-    function spawn(address _owner, uint256 _dna) internal returns (uint256) {
-        require(spanwer[msg.sender], "No permistion");
-        
+    modifier onlyOperator() {
+        require(operator[msg.sender], "No permission");
+        _;
+    }
+
+    function addOperator(address _add) public onlyOwner {
+        require(!operator[_add], "It's operator already");
+        operators.push(_add);
+        operator[_add] = true;
+    }
+
+    function removeOperator(address _add) public onlyOwner {
+        require(operator[_add], "It's not operator");
+        operator[_add] = false;
+        for (uint i = 0; i < operators.length; i++) {
+            if (operators[i] == _add) {
+                operators[i] = operators[operators.length - 1];
+                operators.pop();
+                break;
+            }
+        }
+    }
+
+    function spawn(address _owner, uint256 _dna) public onlyOperator returns (uint256) {
         DNAs.push(_dna);
         
         uint256 newCyblocId = DNAs.length - 1;
@@ -46,46 +60,44 @@ contract MollectorCard is MollectorCardBase {
         return newCyblocId;
     }
 
-    function update(uint _tokenId, uint256 _dna) internal {
-        require(manager[msg.sender], "No permistion");
+    function burn(uint _tokenId) public {
+        _burn(_tokenId);
+    }
+
+    function update(uint _tokenId, uint256 _dna) public onlyOperator {
         DNAs[_tokenId] = _dna;
 
         emit Updated(_tokenId, _dna);
     }
 
-    function link(uint _tokenId, uint _nftLinkIndex, uint _nftLinkTokenId, Proof memory _proof) public {
+    function link(uint _tokenId, uint _nftLinkIndex, uint _nftLinkTokenId) public onlyOperator {
         require(NFTLinks[_nftLinkIndex].add != address(0x0), "Invalid NFTLink");
-        require(ownerOf(_tokenId) == msg.sender, "You are not owner of token");
         
-        
-        bytes memory encode = abi.encodePacked(_tokenId, _nftLinkIndex, _nftLinkTokenId, msg.sender);
-        require(verifyProof(encode, _proof), "Wrong proof");
-
         CardLinks[_tokenId][_nftLinkIndex] = _nftLinkTokenId;
 
         emit Link(_tokenId, _nftLinkIndex, _nftLinkTokenId);
     }
 
-    function fusion(uint _tokenId1, uint _tokenId2) public {
-        require(ownerOf(_tokenId1) == msg.sender && ownerOf(_tokenId2) == msg.sender, "You are not owner of tokens");
+    // function fusion(uint _tokenId1, uint _tokenId2) public {
+    //     require(ownerOf(_tokenId1) == msg.sender && ownerOf(_tokenId2) == msg.sender, "You are not owner of tokens");
 
-        uint newDNA = fusionDNA(DNAs[_tokenId1], DNAs[_tokenId2]);
+    //     uint newDNA = fusionDNA(DNAs[_tokenId1], DNAs[_tokenId2]);
 
-        _update(_tokenId1, newDNA);
-        _burn(_tokenId2);
-    }
+    //     _update(_tokenId1, newDNA);
+    //     _burn(_tokenId2);
+    // }
 
-    function levelUp(uint _tokenId, uint _toLevel) public {
-        require(ownerOf(_tokenId) == msg.sender, "You are not owner of token");
+    // function levelUp(uint _tokenId, uint _toLevel) public {
+    //     require(ownerOf(_tokenId) == msg.sender, "You are not owner of token");
 
-        uint newDNA = levelUpDNA(DNAs[_tokenId], _toLevel);
-        _update(_tokenId, newDNA);
-    }
+    //     uint newDNA = levelUpDNA(DNAs[_tokenId], _toLevel);
+    //     _update(_tokenId, newDNA);
+    // }
 
-    function mutant(uint _tokenId) public {
-        require(ownerOf(_tokenId) == msg.sender, "You are not owner of token");
+    // function mutant(uint _tokenId) public {
+    //     require(ownerOf(_tokenId) == msg.sender, "You are not owner of token");
 
-        uint newDNA = mutantDNA(DNAs[_tokenId]);
-        _update(_tokenId, newDNA);
-    }
+    //     uint newDNA = mutantDNA(DNAs[_tokenId]);
+    //     _update(_tokenId, newDNA);
+    // }
 }
